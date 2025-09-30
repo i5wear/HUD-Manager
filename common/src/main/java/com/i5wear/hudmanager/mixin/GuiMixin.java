97d9317b3f0220a147656b17234209ac09bcbf1c
i5wear <103974588+i5wear@users.noreply.github.com>
2025-09-30 21:50:57 +0800

    Improved options sync

--- common/src/main/java/com/i5wear/hudmanager/mixin/GuiMixin.java
+++ common/src/main/java/com/i5wear/hudmanager/mixin/GuiMixin.java
@@ -13,7 +13,7 @@ import org.spongepowered.asm.mixin.Unique;
 public abstract class GuiMixin {
 
     // Uniform Interface Layer of GUI scale & offset.
-    @Unique static private void HudManager_modifyHudElement(int scale, int x, int y, GuiGraphics arg0, DeltaTracker arg1, Operation<Void> original) {
+    @Unique private static void modifyHudElement(int scale, int x, int y, GuiGraphics arg0, DeltaTracker arg1, Operation<Void> original) {
         arg0.pose().pushMatrix();
         HudManager.Temp.SCALE = scale;
         arg0.pose().scale(0.01f * scale);
@@ -25,101 +25,101 @@ public abstract class GuiMixin {
 
     @WrapMethod(method = "renderOverlayMessage")
     private void modifyActionBar(GuiGraphics arg0, DeltaTracker arg1, Operation<Void> original) {
-        int scale = HudManager.ActionBarScale.getAsInt();
-        if (scale == 0 || HudManager.ActionBarShow.isFalse()) return;
-        int x = HudManager.ActionBarOffsetX.getAsInt();
-        int y = HudManager.ActionBarOffsetY.getAsInt();
-        HudManager_modifyHudElement(scale, x, y, arg0, arg1, original);
+        int scale = HudManager.ActionBarScale.get();
+        if (scale == 0 || HudManager.ActionBarShow.get() == false) return;
+        int x = HudManager.ActionBarOffsetX.get();
+        int y = HudManager.ActionBarOffsetY.get();
+        modifyHudElement(scale, x, y, arg0, arg1, original);
     }
 
     @WrapMethod(method = "renderSavingIndicator")
     private void modifyAutoSaveIndicator(GuiGraphics arg0, DeltaTracker arg1, Operation<Void> original) {
-        int scale = HudManager.AutoSaveIndicatorScale.getAsInt();
-        if (scale == 0 || HudManager.AutoSaveIndicatorShow.isFalse()) return;
-        int x = HudManager.AutoSaveIndicatorOffsetX.getAsInt();
-        int y = HudManager.AutoSaveIndicatorOffsetY.getAsInt();
-        HudManager_modifyHudElement(scale, x, y, arg0, arg1, original);
+        int scale = HudManager.AutoSaveIndicatorScale.get();
+        if (scale == 0 || !HudManager.AutoSaveIndicatorShow.get()) return;
+        int x = HudManager.AutoSaveIndicatorOffsetX.get();
+        int y = HudManager.AutoSaveIndicatorOffsetY.get();
+        modifyHudElement(scale, x, y, arg0, arg1, original);
     }
 
     @WrapMethod(method = "renderBossOverlay")
     private void modifyBossBar(GuiGraphics arg0, DeltaTracker arg1, Operation<Void> original) {
-        int scale = HudManager.BossBarScale.getAsInt();
-        if (scale == 0 || HudManager.BossBarShow.isFalse()) return;
-        int x = HudManager.BossBarOffsetX.getAsInt();
-        int y = HudManager.BossBarOffsetY.getAsInt();
-        HudManager_modifyHudElement(scale, x, y, arg0, arg1, original);
+        int scale = HudManager.BossBarScale.get();
+        if (scale == 0 || !HudManager.BossBarShow.get()) return;
+        int x = HudManager.BossBarOffsetX.get();
+        int y = HudManager.BossBarOffsetY.get();
+        modifyHudElement(scale, x, y, arg0, arg1, original);
     }
 
     @WrapMethod(method = "renderSubtitleOverlay")
     private void modifyClosedCaption(GuiGraphics arg0, DeltaTracker arg1, Operation<Void> original) {
-        int scale = HudManager.ClosedCaptionScale.getAsInt();
-        if (scale == 0 || HudManager.ClosedCaptionShow.isFalse()) return;
-        int x = HudManager.ClosedCaptionOffsetX.getAsInt();
-        int y = HudManager.ClosedCaptionOffsetY.getAsInt();
-        HudManager_modifyHudElement(scale, x, y, arg0, arg1, original);
+        int scale = HudManager.ClosedCaptionScale.get();
+        if (scale == 0 || !HudManager.ClosedCaptionShow.get()) return;
+        int x = HudManager.ClosedCaptionOffsetX.get();
+        int y = HudManager.ClosedCaptionOffsetY.get();
+        modifyHudElement(scale, x, y, arg0, arg1, original);
     }
 
     @WrapMethod(method = "renderCrosshair")
     private void modifyCrosshair(GuiGraphics arg0, DeltaTracker arg1, Operation<Void> original) {
-        int scale = HudManager.CrosshairScale.getAsInt();
-        if (scale == 0 || HudManager.CrosshairShow.isFalse()) return;
-        int x = HudManager.CrosshairOffsetX.getAsInt();
-        int y = HudManager.CrosshairOffsetY.getAsInt();
-        HudManager_modifyHudElement(scale, x, y, arg0, arg1, original);
+        int scale = HudManager.CrosshairScale.get();
+        if (scale == 0 || !HudManager.CrosshairShow.get()) return;
+        int x = HudManager.CrosshairOffsetX.get();
+        int y = HudManager.CrosshairOffsetY.get();
+        modifyHudElement(scale, x, y, arg0, arg1, original);
     }
 
     @WrapMethod(method = "renderDebugOverlay")
     private void modifyDebugScreen(GuiGraphics arg0, DeltaTracker arg1, Operation<Void> original) {
-        int scale = HudManager.DebugScreenScale.getAsInt();
-        if (scale == 0 || HudManager.DebugScreenShow.isFalse()) return;
-        int x = HudManager.DebugScreenOffsetX.getAsInt();
-        int y = HudManager.DebugScreenOffsetY.getAsInt();
-        HudManager_modifyHudElement(scale, x, y, arg0, arg1, original);
+        int scale = HudManager.DebugScreenScale.get();
+        if (scale == 0 || !HudManager.DebugScreenShow.get()) return;
+        int x = HudManager.DebugScreenOffsetX.get();
+        int y = HudManager.DebugScreenOffsetY.get();
+        modifyHudElement(scale, x, y, arg0, arg1, original);
     }
 
     /* This only works on Fabric. This method may be deleted in Neoforge.
     @WrapMethod(method = "renderHotbarAndDecorations")
     private void modifyHotbar(GuiGraphics arg0, DeltaTracker arg1, Operation<Void> original) {
-        int scale = HudManager.HotbarScale.getAsInt();
-        if (scale == 0 || HudManager.HotbarShow.isFalse()) return;
-        int x = HudManager.HotbarOffsetX.getAsInt();
-        int y = HudManager.HotbarOffsetY.getAsInt();
-        HudManager_modifyHudElement(scale, x, y, arg0, arg1, original);
+        int scale = HudManager.HotbarScale.get();
+        if (scale == 0 || !HudManager.HotbarShow.get()) return;
+        int x = HudManager.HotbarOffsetX.get();
+        int y = HudManager.HotbarOffsetY.get();
+        modifyHudElement(scale, x, y, arg0, arg1, original);
     } */
 
     @WrapMethod(method = "renderTabList")
     private void modifyPlayerList(GuiGraphics arg0, DeltaTracker arg1, Operation<Void> original) {
-        int scale = HudManager.PlayerListScale.getAsInt();
-        if (scale == 0 || HudManager.PlayerListShow.isFalse()) return;
-        int x = HudManager.PlayerListOffsetX.getAsInt();
-        int y = HudManager.PlayerListOffsetY.getAsInt();
-        HudManager_modifyHudElement(scale, x, y, arg0, arg1, original);
+        int scale = HudManager.PlayerListScale.get();
+        if (scale == 0 || !HudManager.PlayerListShow.get()) return;
+        int x = HudManager.PlayerListOffsetX.get();
+        int y = HudManager.PlayerListOffsetY.get();
+        modifyHudElement(scale, x, y, arg0, arg1, original);
     }
 
     @WrapMethod(method = "renderScoreboardSidebar")
     private void modifyScoreboardSidebar(GuiGraphics arg0, DeltaTracker arg1, Operation<Void> original) {
-        int scale = HudManager.ScoreboardSidebarScale.getAsInt();
-        if (scale == 0 || HudManager.ScoreboardSidebarShow.isFalse()) return;
-        int x = HudManager.ScoreboardSidebarOffsetX.getAsInt();
-        int y = HudManager.ScoreboardSidebarOffsetY.getAsInt();
-        HudManager_modifyHudElement(scale, x, y, arg0, arg1, original);
+        int scale = HudManager.ScoreboardSidebarScale.get();
+        if (scale == 0 || !HudManager.ScoreboardSidebarShow.get()) return;
+        int x = HudManager.ScoreboardSidebarOffsetX.get();
+        int y = HudManager.ScoreboardSidebarOffsetY.get();
+        modifyHudElement(scale, x, y, arg0, arg1, original);
     }
 
     @WrapMethod(method = "renderTitle")
     private void modifyScreenTitle(GuiGraphics arg0, DeltaTracker arg1, Operation<Void> original) {
-        int scale = HudManager.ScreenTitleScale.getAsInt();
-        if (scale == 0 || HudManager.ScreenTitleShow.isFalse()) return;
-        int x = HudManager.ScreenTitleOffsetX.getAsInt();
-        int y = HudManager.ScreenTitleOffsetY.getAsInt();
-        HudManager_modifyHudElement(scale, x, y, arg0, arg1, original);
+        int scale = HudManager.ScreenTitleScale.get();
+        if (scale == 0 || !HudManager.ScreenTitleShow.get()) return;
+        int x = HudManager.ScreenTitleOffsetX.get();
+        int y = HudManager.ScreenTitleOffsetY.get();
+        modifyHudElement(scale, x, y, arg0, arg1, original);
     }
 
     @WrapMethod(method = "renderEffects")
     private void modifyStatusEffect(GuiGraphics arg0, DeltaTracker arg1, Operation<Void> original) {
-        int scale = HudManager.StatusEffectScale.getAsInt();
-        if (scale == 0 || HudManager.StatusEffectShow.isFalse()) return;
-        int x = HudManager.StatusEffectOffsetX.getAsInt();
-        int y = HudManager.StatusEffectOffsetY.getAsInt();
-        HudManager_modifyHudElement(scale, x, y, arg0, arg1, original);
+        int scale = HudManager.StatusEffectScale.get();
+        if (scale == 0 || !HudManager.StatusEffectShow.get()) return;
+        int x = HudManager.StatusEffectOffsetX.get();
+        int y = HudManager.StatusEffectOffsetY.get();
+        modifyHudElement(scale, x, y, arg0, arg1, original);
     }
 }
--- common/src/main/java/com/i5wear/hudmanager/mixin/OptionsMixin.java
+++ common/src/main/java/com/i5wear/hudmanager/mixin/OptionsMixin.java
@@ -5,17 +5,32 @@ import com.llamalad7.mixinextras.injector.ModifyReturnValue;
 import net.minecraft.client.OptionInstance;
 import net.minecraft.client.Options;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 
 @Mixin(Options.class)
-public class OptionsMixin {
+public abstract class OptionsMixin {
 
-    @ModifyReturnValue(method = "showSubtitles", at = @At("RETURN"))
-    public OptionInstance<Boolean> modifySubtitlesShow(OptionInstance<Boolean> original)
-    { original.set(HudManager.ClosedCaptionShow.getAsBoolean()); return original; }
+    @Unique private static boolean AutosaveIndicatorFlag = HudManager.AutoSaveIndicatorShow.get();
+    @Unique private static boolean ClosedCaptionFlag = HudManager.ClosedCaptionShow.get();
 
     @ModifyReturnValue(method = "showAutosaveIndicator", at = @At("RETURN"))
-    public OptionInstance<Boolean> modifyAutosaveIndicatorShow(OptionInstance<Boolean> original)
-    { original.set(HudManager.AutoSaveIndicatorShow.getAsBoolean()); return original; }
+    public OptionInstance<Boolean> modifyAutosaveIndicatorShow(OptionInstance<Boolean> original) {
+        if (original.get() != HudManager.AutoSaveIndicatorShow.get()) {
+            original.set(!AutosaveIndicatorFlag);
+            HudManager.AutoSaveIndicatorShow.set(!AutosaveIndicatorFlag);
+            AutosaveIndicatorFlag = !AutosaveIndicatorFlag;
+        }
+        return original;
+    }
 
+    @ModifyReturnValue(method = "showSubtitles", at = @At("RETURN"))
+    public OptionInstance<Boolean> modifySubtitlesShow(OptionInstance<Boolean> original) {
+        if (original.get() != HudManager.ClosedCaptionShow.get()) {
+            original.set(!ClosedCaptionFlag);
+            HudManager.ClosedCaptionShow.set(!ClosedCaptionFlag);
+            ClosedCaptionFlag = !ClosedCaptionFlag;
+        }
+        return original;
+    }
 }
