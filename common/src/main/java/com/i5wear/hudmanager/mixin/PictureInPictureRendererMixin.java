f787e45edd7df76a640965f4198ab3aca4e103e5
i5wear <103974588+i5wear@users.noreply.github.com>
2025-11-10 18:49:43 +0800

    Fix oversized item

--- common/src/main/java/com/i5wear/hudmanager/mixin/PictureInPictureRendererMixin.java
+++ common/src/main/java/com/i5wear/hudmanager/mixin/PictureInPictureRendererMixin.java
@@ -5,21 +5,32 @@ import com.llamalad7.mixinextras.sugar.Local;
 import net.minecraft.client.gui.render.pip.PictureInPictureRenderer;
 import net.minecraft.client.gui.render.state.BlitRenderState;
 import net.minecraft.client.gui.render.state.pip.GuiProfilerChartRenderState;
+import net.minecraft.client.gui.render.state.pip.OversizedItemRenderState;
 import net.minecraft.client.gui.render.state.pip.PictureInPictureRenderState;
 import org.joml.Matrix3x2f;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyArg;
 
+import java.util.function.Supplier;
+
 @Mixin(PictureInPictureRenderer.class)
 public abstract class PictureInPictureRendererMixin {
 
     @ModifyArg(method = "blitTexture", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/render/state/GuiRenderState;submitBlitToCurrentLayer(Lnet/minecraft/client/gui/render/state/BlitRenderState;)V"))
-    private BlitRenderState modifyProfilerChart(BlitRenderState original, @Local(ordinal = 0, argsOnly = true) PictureInPictureRenderState instance) {
-        return !(instance instanceof GuiProfilerChartRenderState) ? original : new BlitRenderState(
-            original.pipeline(), original.textureSetup(), new Matrix3x2f().scale(0.01f * Manager.DEBUG_SCREEN.Scale.get()),
-            original.x0(), original.y0(), original.x1(), original.y1(), original.u0(), original.u1(), original.v0(), original.v1(),
-            Manager.modifyColor(0xFFFFFFFF, Manager.DEBUG_SCREEN.Opacity.get()), original.scissorArea()
-        );
+    private BlitRenderState modifySpecialElement(BlitRenderState original, @Local(ordinal = 0, argsOnly = true) PictureInPictureRenderState instance) {
+        return switch (instance) {
+            case GuiProfilerChartRenderState ignored -> new BlitRenderState(
+                original.pipeline(), original.textureSetup(), original.pose().scale(0.01f * Manager.DEBUG_SCREEN.Scale.get(), new Matrix3x2f()),
+                original.x0(), original.y0(), original.x1(), original.y1(), original.u0(), original.u1(), original.v0(), original.v1(),
+                Manager.modifyColor(0xFFFFFFFF, Manager.DEBUG_SCREEN.Opacity.get()), original.scissorArea()
+            );
+            case OversizedItemRenderState ignored -> new BlitRenderState(
+                original.pipeline(), original.textureSetup(), original.pose(),
+                original.x0(), original.y0(), original.x1(), original.y1(), original.u0(), original.u1(), original.v0(), original.v1(),
+                ((Supplier<Integer>)(Object)(ignored.guiItemRenderState())).get(), original.scissorArea()
+            );
+            default -> original;
+        };
     }
 }
