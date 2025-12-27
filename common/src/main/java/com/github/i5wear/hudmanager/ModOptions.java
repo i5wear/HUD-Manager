0500a9b782e5ea64bc9f813f8d78298f0fe07bd4
i5wear <103974588+i5wear@users.noreply.github.com>
2025-12-27 22:34:50 +0800

    fixed typo

--- common/src/main/java/com/github/i5wear/hudmanager/ModOptions.java
+++ common/src/main/java/com/github/i5wear/hudmanager/ModOptions.java
@@ -8,7 +8,7 @@ import java.nio.file.Path;
 
 /**
  * <p> A simple config implementation. </p>
- * <p> Contains arbitrary objects that convert to, and get values from {@link Gson} or {@link ModOptionsScreen}. </p>
+ * <p> Contains arbitrary objects that convert to, and get values from JSON or {@link ModOptionsScreen}. </p>
  * <p> Supports {@link Number}, {@link String}, {@link Boolean} and {@link Enum} types, or their collections and combinations. </p>
  * <p> For special cases, register your own type adapter at {@link #ADAPTER} and it works for {@link ModOptionsScreen}. </p>
  * <p> Requires runtime initialization of the config path, before calling method {@link #load()} and {@link #save()}. </p>
