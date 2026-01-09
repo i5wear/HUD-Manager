9b161c6c3e16a4e187b66817c4049fe142486ddf
i5wear <103974588+i5wear@users.noreply.github.com>
2026-01-09 17:37:46 +0800

    Update GuiRenderStateMixin.java

--- common/src/main/java/com/github/i5wear/hudmanager/mixin/GuiRenderStateMixin.java
+++ common/src/main/java/com/github/i5wear/hudmanager/mixin/GuiRenderStateMixin.java
@@ -1,19 +1,19 @@
 package com.github.i5wear.hudmanager.mixin;
 
 import com.github.i5wear.hudmanager.HudManager;
+import com.llamalad7.mixinextras.injector.wrapmethod.WrapMethod;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import net.minecraft.client.gui.render.state.GuiRenderState;
 import net.minecraft.client.gui.render.state.pip.PictureInPictureRenderState;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.ModifyVariable;
 
 @Mixin(GuiRenderState.class)
 public abstract class GuiRenderStateMixin {
 
-    @ModifyVariable(method = "submitPicturesInPictureState", at = @At("HEAD"), ordinal = 0, argsOnly = true)
-    private PictureInPictureRenderState storeExtraElement(PictureInPictureRenderState original) {
+    @WrapMethod(method = "submitPicturesInPictureState")
+    private void storeExtraElement(PictureInPictureRenderState input, Operation<Void> original) {
         if (HudManager.CURRENT != HudManager.DEFAULT)
-            HudManager.STORAGE.put(original, HudManager.CURRENT);
-        return original;
+            HudManager.STORAGE.put(input, HudManager.CURRENT);
+        original.call(input);
     }
 }
