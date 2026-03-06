ad616ea1821d58e926b091fab3084999bd561651
i5wear <103974588+i5wear@users.noreply.github.com>
2026-03-06 20:04:04 +0800

    Added more repos

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,12 +2,6 @@ plugins {
     `java-library`
 }
 
-subprojects {
-    repositories {
-        maven { url = uri("https://maven.terraformersmc.com/releases") }
-    }
-}
-
 project.version = libs.versions.project.get()
 
 tasks.withType<Jar>().configureEach {
@@ -15,4 +9,13 @@ tasks.withType<Jar>().configureEach {
     from(project(":common").sourceSets.main.get().output)
     from(project(":fabric").sourceSets.main.get().output)
     from(project(":neoforge").sourceSets.main.get().output)
+}
+
+subprojects {
+    repositories {
+        maven { url = uri("https://maven.terraformersmc.com/releases/") }
+        maven { url = uri("https://api.modrinth.com/maven/") }
+        maven { url = uri("https://cursemaven.com/") }
+        maven { url = uri("https://jitpack.io/") }
+    }
 }
