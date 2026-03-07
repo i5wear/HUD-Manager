742b59c6efd470071b4919707b420b24e5c68ef8
i5wear <103974588+i5wear@users.noreply.github.com>
2026-03-07 14:50:11 +0800

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,7 +1,7 @@
 pluginManagement {
+    repositories.maven { url = uri("https://plugins.gradle.org/m2/") }
     repositories.maven { url = uri("https://maven.fabricmc.net/") }
     repositories.maven { url = uri("https://maven.neoforged.net/releases/") }
-    repositories.gradlePluginPortal()
 }
 
 rootProject.name = "hudmanager"
