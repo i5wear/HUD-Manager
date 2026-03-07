d895e54b78e125c36b2a5ba32fc38bea4bb2cf37
i5wear <103974588+i5wear@users.noreply.github.com>
2026-03-07 12:54:49 +0800

    Reordered some content

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,8 +1,10 @@
+rootProject.name = "hudmanager"
+include("common", "fabric", "neoforge")
+
 pluginManagement.repositories {
     maven { url = uri("https://maven.fabricmc.net/") }
     maven { url = uri("https://maven.neoforged.net/releases/") }
     gradlePluginPortal()
 }
 
-rootProject.name = "hudmanager"
-include("common", "fabric", "neoforge")
+//dependencyResolutionManagement.repositories {}
