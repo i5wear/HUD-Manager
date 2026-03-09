5c01c307ba639cf1bc57d03c1044e7d0415b5099
i5wear <103974588+i5wear@users.noreply.github.com>
2026-03-09 11:05:05 +0800

    Update build.gradle.kts

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -2,6 +2,11 @@ plugins {
     alias(libs.plugins.fabric.loom)
 }
 
+repositories {
+    maven { url = uri("https://api.modrinth.com/maven/") }
+    maven { url = uri("https://cursemaven.com/") }
+}
+
 dependencies {
     minecraft(libs.minecraft)
     compileOnly(libs.fabric.loader)
