ec48a0a7aa9743c14d026601582021a17d428d41
i5wear <103974588+i5wear@users.noreply.github.com>
2026-03-13 16:39:07 +0800

    Update WindowMixin.java

--- common/src/main/java/com/github/i5wear/hudmanager/mixin/WindowMixin.java
+++ common/src/main/java/com/github/i5wear/hudmanager/mixin/WindowMixin.java
@@ -1,19 +1,18 @@
 package com.github.i5wear.hudmanager.mixin;
 
 import com.github.i5wear.hudmanager.HudManager;
-import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
+import com.llamalad7.mixinextras.injector.ModifyReturnValue;
 import com.mojang.blaze3d.platform.Window;
-import org.objectweb.asm.Opcodes;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 
 @Mixin(Window.class)
 public abstract class WindowMixin {
 
-    @ModifyExpressionValue(method = "getGuiScaledWidth", at = @At(value = "FIELD", opcode = Opcodes.GETFIELD, target = "Lcom/mojang/blaze3d/platform/Window;guiScaledWidth:I"))
+    @ModifyReturnValue(method = "getGuiScaledWidth", at = @At(value = "TAIL"))
     private int storeElementAxisX(int original) { return Math.round(original / HudManager.CURRENT.Resizer); }
 
-    @ModifyExpressionValue(method = "getGuiScaledHeight", at = @At(value = "FIELD", opcode = Opcodes.GETFIELD, target = "Lcom/mojang/blaze3d/platform/Window;guiScaledHeight:I"))
+    @ModifyReturnValue(method = "getGuiScaledHeight", at = @At(value = "TAIL"))
     private int storeElementAxisY(int original) { return Math.round(original / HudManager.CURRENT.Resizer); }
 
 }
