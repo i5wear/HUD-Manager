a37d53ae3842c713876a21a4da5de8f303c9e721
i5wear <103974588+i5wear@users.noreply.github.com>
2026-03-31 14:52:29 +0800

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,18 +10,14 @@ java {
 
 tasks.named<Jar>("jar") {
     from(project.file("LICENSE"))
-    from(project(":common").sourceSets.main.get().output)
-    from(project(":fabric").sourceSets.main.get().output)
-    from(project(":neoforge").sourceSets.main.get().output)
+    subprojects.forEach { from(it.sourceSets.main.get().output) }
     filesMatching("**/*.json") { expand(mapOf("version" to project.version)) }
     filesMatching("**/*.toml") { expand(mapOf("version" to project.version)) }
 }
 
 tasks.named<Jar>("sourcesJar") {
     from(project.file("LICENSE"))
-    from(project(":common").sourceSets.main.get().allSource)
-    from(project(":fabric").sourceSets.main.get().allSource)
-    from(project(":neoforge").sourceSets.main.get().allSource)
+    subprojects.forEach { from(it.sourceSets.main.get().allSource) }
     filesMatching("**/*.json") { expand(mapOf("version" to project.version)) }
     filesMatching("**/*.toml") { expand(mapOf("version" to project.version)) }
 }
