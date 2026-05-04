94c021db2fac9ae59a2abfa1ae66f3bcf2c41ddc
i5wear <103974588+i5wear@users.noreply.github.com>
2026-05-04 21:10:42 +0800

    fixes #29

--- common/src/main/java/com/github/i5wear/hudmanager/mixin/GuiRenderStateMixin.java
+++ common/src/main/java/com/github/i5wear/hudmanager/mixin/GuiRenderStateMixin.java
@@ -30,7 +30,7 @@ public abstract class GuiRenderStateMixin {
         return new BlitRenderState(
             original.pipeline(), original.textureSetup(), HudManager.CURRENT.apply(original.pose()),
             original.x0(), original.y0(), original.x1(), original.y1(), original.u0(), original.u1(), original.v0(), original.v1(),
-            ARGB.srgbLerp(HudManager.CURRENT.Opacity, 0, original.color()), original.scissorArea(), original.bounds()
+            ARGB.srgbLerp(HudManager.CURRENT.Opacity, 0, original.color()), original.scissorArea()
         );
     }
 
@@ -49,17 +49,17 @@ public abstract class GuiRenderStateMixin {
             case ColoredRectangleRenderState output -> new ColoredRectangleRenderState(
                 output.pipeline(), output.textureSetup(), HudManager.CURRENT.apply(output.pose()),
                 output.x0(), output.y0(), output.x1(), output.y1(), ARGB.multiplyAlpha(output.col1(), HudManager.CURRENT.Opacity),
-                ARGB.multiplyAlpha(output.col2(), HudManager.CURRENT.Opacity), output.scissorArea(), output.bounds()
+                ARGB.multiplyAlpha(output.col2(), HudManager.CURRENT.Opacity), output.scissorArea()
             );
             case BlitRenderState output -> new BlitRenderState(
                 output.pipeline(), output.textureSetup(), HudManager.CURRENT.apply(output.pose()),
                 output.x0(), output.y0(), output.x1(), output.y1(), output.u0(), output.u1(), output.v0(), output.v1(),
-                ARGB.multiplyAlpha(output.color(), HudManager.CURRENT.Opacity), output.scissorArea(), output.bounds()
+                ARGB.multiplyAlpha(output.color(), HudManager.CURRENT.Opacity), output.scissorArea()
             );
             case TiledBlitRenderState output -> new TiledBlitRenderState(
                 output.pipeline(), output.textureSetup(), HudManager.CURRENT.apply(output.pose()), output.tileWidth(), output.tileHeight(),
                 output.x0(), output.y0(), output.x1(), output.y1(), output.u0(), output.u1(), output.v0(), output.v1(),
-                ARGB.multiplyAlpha(output.color(), HudManager.CURRENT.Opacity), output.scissorArea(), output.bounds()
+                ARGB.multiplyAlpha(output.color(), HudManager.CURRENT.Opacity), output.scissorArea()
             );
             default -> original;
         };
